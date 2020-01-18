058efb3712165256065080af5a9d51a0301f0d1b
m.rescati1 <m.rescati@gmail.com>
2020-01-18 14:42:34 +0100

    Converter First Test

---
+++ myThread.py
@@ -0,0 +1,20 @@
+import threading
+import time
+import subprocess
+
+exitFlag = 0
+
+def convert_video(videoName):
+    print("converting " + videoName )
+    subprocess.run("bash convert.sh " + videoName)
+    subprocess.run("rm ../recordings/" + videoName)
+
+class convThread (threading.Thread):
+   def __init__(self, threadID, videoName):
+      threading.Thread.__init__(self)
+      self.threadID = threadID
+      self.videoName = videoName
+   def run(self):
+      print "Starting " + self.threadID
+      convert_video(self.videoName)
+      print "Exiting " + self.threadID
--- video.py
+++ video.py
@@ -1,14 +1,21 @@
 import cv2
 import time as t
 import datetime as date
+import convThread from myThread.py
 
-cap = cv2.VideoCapture("rtsp://192.168.1.21:554/11") 
-vid = cv2.VideoWriter_fourcc('M', 'J', 'P', 'G')
-#vid = cv2.VideoWriter_fourcc('X', '2', '6', '4')
-while(True):
+def setVideoName():
     dt=date.datetime.now()
     h=dt.strftime('%Y-%m-%d-%H-%M')
-    output = cv2.VideoWriter("/home/pi/recordings/"+ h+ "-record.avi", vid, 25, (1280,720))
+    videoName= h + "-record.avi"
+    return videoName
+
+
+cap = cv2.VideoCapture("rtsp://192.168.1.21:554/11")
+vid = cv2.VideoWriter_fourcc('M', 'J', 'P', 'G')
+iD=0
+while(True):
+    videoName=setVideoName()
+    output = cv2.VideoWriter("/home/pi/recordings/"+ videoName, vid, 25, (1280,720))
     start=t.time()
     while (cap.isOpened()):
         ret, frame= cap.read()
@@ -16,6 +23,10 @@ while(True):
         now= t.time()
         if(now > start + 60*10):
             break
+    converter=convThread( iD , videoName)
+    converter.start()
+    iD+=0
+
 cap.release()
 
 
